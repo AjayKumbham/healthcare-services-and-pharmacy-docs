fc2b80373fe9adf08839f6ecea59c63223e30975
Kumbham Ajay Goud <ajaygoud.kumbham@gmail.com>
2025-07-31 14:13:37 +0530

    chore: update package dependencies and configure Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/healthcare-services-and-pharmacy-docs/',
   server: {
     host: "::",
     port: 8080,
